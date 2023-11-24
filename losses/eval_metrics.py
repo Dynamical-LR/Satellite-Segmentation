636541b8f5fc5fffbc2344af9b07348995098d1f
= <kirklimushin@gmail.com>
2023-11-24 15:07:04 +0300

    updated losses, network training pipeline, evaluation metrics

--- losses/eval_metrics.py
+++ losses/eval_metrics.py
@@ -15,12 +15,16 @@ class F1Score(nn.Module):
     Implementation of the Averaged F1 Score metric
     """
     def _calculate_recall(self, pred_mask, mask):
-        pass 
+        tp = len(pred_mask[pred_mask == mask == 1].flatten())
+        fn = len(pred_mask[pred_mask != mask == 1].flatten())  # Fix: Count false negatives correctly
+        return tp / (fn + tp)
 
     def _calculate_precision(self, pred_mask, mask):
-        pass 
+        tp = len(pred_mask[pred_mask == mask == 1].flatten())
+        fp = len(pred_mask[pred_mask != mask == 0].flatten())  # Fix: Count false positives correctly
+        return tp / (fp + tp)
 
     def forward(self, predicted_mask, mask):
         recall = self._calculate_recall(predicted_mask, mask)
         precision = self._calculate_precision(predicted_mask, mask)
-        return (2 * precision * recall) / precision + recall
+        return (2 * precision * recall) / (precision + recall)
